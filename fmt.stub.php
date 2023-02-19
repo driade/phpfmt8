8eaa6885a8e44fa9d6c56e593deabf6433411eaf
David Fernández <david@mundosaparte.com>
2023-02-19 14:34:17 +0100

    remove the no longer working self update option

--- fmt.stub.php
+++ fmt.stub.php
@@ -1465,45 +1465,6 @@ namespace {
 		return $final;
 	}
 
-	function selfupdate($argv, $inPhar) {
-		$opts = [
-			'http' => [
-				'method' => 'GET',
-				'header' => "User-agent: phpfmt fmt.phar selfupdate\r\n",
-			],
-		];
-
-		$context = stream_context_create($opts);
-
-		$releases = json_decode(file_get_contents('https://api.github.com/repos/phpfmt/fmt/tags', false, $context), true);
-		$commit = json_decode(file_get_contents($releases[0]['commit']['url'], false, $context), true);
-		$files = json_decode(file_get_contents($commit['commit']['tree']['url'], false, $context), true);
-		foreach ($files['tree'] as $file) {
-			if ('fmt.phar' == $file['path']) {
-				$phar_file = base64_decode(json_decode(file_get_contents($file['url'], false, $context), true)['content']);
-			}
-			if ('fmt.phar.sha1' == $file['path']) {
-				$phar_sha1 = base64_decode(json_decode(file_get_contents($file['url'], false, $context), true)['content']);
-			}
-		}
-		if (!isset($phar_sha1) || !isset($phar_file)) {
-			fwrite(STDERR, 'Could not autoupdate - not release found' . PHP_EOL);
-			exit(255);
-		}
-		if ($inPhar && !file_exists($argv[0])) {
-			$argv[0] = dirname(Phar::running(false)) . DIRECTORY_SEPARATOR . $argv[0];
-		}
-		if (sha1_file($argv[0]) != $phar_sha1) {
-			copy($argv[0], $argv[0] . '~');
-			file_put_contents($argv[0], $phar_file);
-			chmod($argv[0], 0777 & ~umask());
-			fwrite(STDERR, 'Updated successfully' . PHP_EOL);
-			exit(0);
-		}
-		fwrite(STDERR, 'Up-to-date!' . PHP_EOL);
-		exit(0);
-	}
-
 	define('ST_AT', '@');
 	define('ST_BRACKET_CLOSE', ']');
 	define('ST_BRACKET_OPEN', '[');
@@ -12946,7 +12907,6 @@ EOT;
 				'-v' => 'verbose',
 			];
 			if ($inPhar) {
-				$options['--selfupdate'] = 'self-update fmt.phar from Github';
 				$options['--version'] = 'version';
 			}
 			$options['--cache[=FILENAME]'] .= (Cacher::DEFAULT_CACHE_FILENAME);
@@ -12994,7 +12954,6 @@ EOT;
 			'yoda',
 		];
 		if ($inPhar) {
-			$getoptLongOptions[] = 'selfupdate';
 			$getoptLongOptions[] = 'version';
 		}
 		if (!$enableCache) {
