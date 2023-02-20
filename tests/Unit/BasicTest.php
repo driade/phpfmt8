7efdf9775a77f10c71aa62489979729133518326
David Fernández <david@mundosaparte.com>
2023-02-20 20:16:13 +0100

    fix invalid way of converting the return carriages on windows

--- tests/Unit/BasicTest.php
+++ tests/Unit/BasicTest.php
@@ -10,8 +10,7 @@ class BasicTest extends TestCase
 
         $file = file_get_contents(__DIR__ . '/fixtures/one.php');
         if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-            $output = str_replace(["\r", "\n"], "\r\n", $output);
-            $output = preg_replace("/\r\n{2,}/", "\r\n\r\n", $output);
+            $output = str_replace("\n", "\r\n", $output);
         }
 
         $this->assertSame($file, implode("\n", $output));
