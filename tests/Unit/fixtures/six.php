898f1ac543b17c4cb12a6fb576a34b39eb213d1c
David Fernández <david@mundosaparte.com>
2023-02-22 16:00:17 +0100

    fix PSR2ModifierVisibilityStaticOrder https://github.com/driade/phpfmt-8/issues/5

--- tests/Unit/fixtures/six.php
+++ tests/Unit/fixtures/six.php
@@ -1,5 +1,10 @@
 <?php
 
-class FOO {
-    public string $a = 'b';
+abstract class FOO {
+	public string $a = 'b';
+	public static float $b = 0.0;
+	abstract protected function zim();
+	final public static function bar() {
+
+	}
 }
