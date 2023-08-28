9641034bdc0cb047629533c53411cb4f1f54b13f
David Fernández <david@mundosaparte.com>
2023-08-28 18:43:35 +0200

    fix - tests

--- tests/run_all_tests.php
+++ tests/run_all_tests.php
@@ -161,6 +161,7 @@ foreach ($cases as $caseIn) {
         $expected = file_get_contents($caseOut);
     }
     if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+        $got = str_replace(PHP_EOL, "\n", $got);
         $expected = str_replace(PHP_EOL, "\n", $expected);
     }
 
