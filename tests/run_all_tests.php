f25a156df10b47e1b600aafc230d2b4dabfd8651
David Fernández <david@mundosaparte.com>
2023-08-28 19:01:44 +0200

    no message

--- tests/run_all_tests.php
+++ tests/run_all_tests.php
@@ -161,7 +161,7 @@ foreach ($cases as $caseIn) {
         $expected = file_get_contents($caseOut);
     }
     if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-        $expected = str_replace("\r\n", "\n", $expected);
+        $got = str_replace(PHP_EOL, "\n", $got);
     }
 
     if ($got != $expected) {
