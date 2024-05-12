11c8126678144811c9857d5c114a4ac8f18ca33d
David Fernández <david@mundosaparte.com>
2024-05-12 19:01:18 +0200

    refact - disable external passes as it's long they doesn't work

--- fmt.stub.php
+++ fmt.stub.php
@@ -2767,10 +2767,6 @@ namespace {
 			}
 
 			if (!class_exists($pass)) {
-				$passName = sprintf('ExternalPass%s', $pass);
-				$passes = array_reverse($this->passes, true);
-				$passes[$passName] = new ExternalPass($pass);
-				$this->passes = array_reverse($passes, true);
 				return;
 			}
 
@@ -3436,55 +3432,6 @@ namespace {
 			}
 
 			return $ret;
-		}
-	}
-
-	class ExternalPass {
-		private $passName = '';
-
-		public function __construct($passName) {
-			$this->passName = $passName;
-		}
-
-		public function candidate() {
-			return !empty($this->passName);
-		}
-
-		public function format($source) {
-			$descriptorspec = [
-				0 => ['pipe', 'r'], 1 => ['pipe', 'w'], 2 => ['pipe', 'w']];
-
-			$cwd = getcwd();
-			$env = [];
-			$argv = $_SERVER['argv'];
-			$pipes = null;
-
-			$external = str_replace('fmt.', 'fmt-external.', $cwd . DIRECTORY_SEPARATOR . $argv[0]);
-
-			$cmd = $_SERVER['_'] . ' ' . $external . ' --pass=' . $this->passName;
-			$process = proc_open(
-				$cmd,
-				$descriptorspec,
-				$pipes,
-				$cwd,
-				$env
-			);
-			if (!is_resource($process)) {
-				fclose($pipes[0]);
-				fclose($pipes[1]);
-				fclose($pipes[2]);
-				proc_close($process);
-				return $source;
-			}
-			fwrite($pipes[0], $source);
-			fclose($pipes[0]);
-
-			$source = stream_get_contents($pipes[1]);
-			fclose($pipes[1]);
-
-			fclose($pipes[2]);
-			proc_close($process);
-			return $source;
 		}
 	}
 
