7e6af476fb2e3e17d3cc99ea46fda6939d8899ab
David Fernández <david@mundosaparte.com>
2025-06-23 19:57:42 +0200

    fix - allow all the reserved works as parameters - https://github.com/kokororin/vscode-phpfmt/issues/174

--- fmt.stub.php
+++ fmt.stub.php
@@ -4790,6 +4790,32 @@ EOT;
 	}
 
 	class ResizeSpaces extends FormatterPass {
+        
+        public static $reservedWords = [
+            '__halt_compiler' => 1,
+            'abstract' => 1, 'and' => 1, 'array' => 1, 'as' => 1,
+            'break' => 1,
+            'callable' => 1, 'case' => 1, 'catch' => 1, 'class' => 1, 'clone' => 1, 'const' => 1, 'continue' => 1,
+            'declare' => 1, 'default' => 1, 'die' => 1, 'do' => 1,
+            'echo' => 1, 'else' => 1, 'elseif' => 1, 'empty' => 1, 'enddeclare' => 1, 'endfor' => 1, 'endforeach' => 1, 'endif' => 1, 'endswitch' => 1, 'endwhile' => 1, 'eval' => 1, 'exit' => 1, 'extends' => 1,
+            'final' => 1, 'for' => 1, 'foreach' => 1, 'function' => 1,
+            'global' => 1, 'goto' => 1,
+            'if' => 1, 'implements' => 1, 'include' => 1, 'include_once' => 1, 'instanceof' => 1, 'insteadof' => 1, 'interface' => 1, 'isset' => 1,
+            'list' => 1,
+            'namespace' => 1, 'new' => 1,
+            'or' => 1,
+            'print' => 1, 'private' => 1, 'protected' => 1, 'public' => 1,
+            'require' => 1, 'require_once' => 1, 'return' => 1,
+            'static' => 1, 'switch' => 1,
+            'throw' => 1, 'trait' => 1, 'try' => 1,
+            'unset' => 1, 'use' => 1, 'var' => 1,
+            'while' => 1, 'xor' => 1,
+            'match' => 1, 'readonly' => 1,
+            'enum' => 1, 'yield' => 1, "yield from" => 1,
+            "true" => 1, "false" => 1, "null" => false,
+            "self" => 1, "parent" => 1, "static" =>1
+        ];
+
         public function candidate($source, $foundTokens) {
             $tkns = token_get_all($source);
 
@@ -4973,7 +4999,7 @@ EOT;
                     break;
 
                 case ST_COLON:
-                    list($prevId) = $this->inspectToken(-1);
+                    list($prevId, $prevText) = $this->inspectToken(-1);
                     list($nextId, $nextText) = $this->inspectToken(+1);
 
                     if (
@@ -5022,7 +5048,7 @@ EOT;
                     } elseif (T_STRING === $prevId) {
                         $this->appendCode($text . ' ');
                         break;
-                    } elseif (T_CLASS === $prevId) {
+                    } elseif (isset(static::$reservedWords[strtolower($prevText)])) {
                         $this->appendCode($text . ' ');
                         break;
                     }
